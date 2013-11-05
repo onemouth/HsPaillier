7ebe5c2282d3db9248e5e2ac866f560f846d9a64
Li Ting Tsai <and.liting@gmail.com>
2013-11-05 09:16:22 +0800

    test CmdArgs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+import System.Console.CmdArgs
+
+data Sample = Sample {hello :: String}
+    deriving (Show, Data, Typeable)
+
+sample = Sample{hello = def}
+sample2 = Sample {hello = def &= help "Who to say hello to" &= typ "WORLD"}
 
 
 main = do
-    putStrLn "test"
+    print =<< cmdArgs sample2
+    
