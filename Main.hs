9234579da95ddea34616322ee235b4cd29fb7f65
Louis Tsai (RD-TW) <louis_tsai@trend.com.tw>
2016-12-15 16:36:46 +0800

    update a small example for debug

--- Main.hs
+++ Main.hs
@@ -12,15 +12,17 @@ sample = Sample {encrypt = def &= help "which integer to encrypt" &= typ "0"}
 
 main = do
     --print =<< cmdArgs sample
-    (pubKey, prvKey) <- P.genKey 256
+    (pubKey, prvKey) <- P.genKey 10
     print (pubKey, prvKey)
     let p = 2 :: Integer
     c <- P.encrypt pubKey p
-    putStrLn $ "ciphertext: " ++ show c
-    --putStrLn $ "plaintext: " ++ show (P.decrypt prvKey pubKey c)
-    let p' = 100 :: Integer
+    putStrLn $ "ciphertext 1: " ++ show c
+    putStrLn $ "plaintext 1: " ++ show (P.decrypt prvKey pubKey c)
+    let p' = 3 :: Integer
     c' <- P.encrypt pubKey p'
-    putStrLn $ "ciphertext: " ++ show c'
+    putStrLn $ "ciphertext 2: " ++ show c'
+    putStrLn $ "plaintext 2: " ++ show (P.decrypt prvKey pubKey c')
     let c'' = P.cipherMul pubKey c c'
-    putStrLn $ "plaintext: " ++ show (P.decrypt prvKey pubKey c'')
+    putStrLn $ "ciphertext (1*2): " ++ show c''
+    putStrLn $ "plaintext (1+2): " ++ show (P.decrypt prvKey pubKey c'')
     
